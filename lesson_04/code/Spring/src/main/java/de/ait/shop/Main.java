bec53ee9f9e2b8ac78c78ca7e275b4fe62bb8e77
Wladimir Weizen <weizen.w@gmx.de>
2023-09-26 15:52:40 +0200

    initial commit

---
+++ lesson_04/code/Spring/src/main/java/de/ait/shop/Main.java
@@ -0,0 +1,54 @@
+package de.ait.shop;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import de.ait.shop.models.User;
+import de.ait.shop.repositories.impl.UsersRepositoryJdbcImpl;
+
+import javax.sql.DataSource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 9/25/2023
+ * Spring
+ *
+ * @author Marsel Sidikov (AIT TR)
+ */
+public class Main {
+    public static void main(String[] args) {
+        // создаем конфигурацию ConnectionPool от Hikari
+        HikariConfig config = new HikariConfig();
+        // указываем URL-подключения
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/shop_db");
+        // настройки пользователя для подключения
+        config.setUsername("postgres");
+        config.setPassword("root");
+        // размер пула (сколько потоков и сколько Connection-ов)
+        config.setMaximumPoolSize(50);
+
+        // создаем объект DataSource
+        DataSource dataSource = new HikariDataSource(config);
+
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop_db");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("qwerty007");
+
+        UsersRepositoryJdbcImpl usersRepository = new UsersRepositoryJdbcImpl(dataSource);
+
+        try (ExecutorService service = Executors.newFixedThreadPool(10)) {
+            for (int i = 0; i < 10; i++) {
+                service.submit(() -> {
+                    System.out.println("task submit");
+                    for (int j = 0; j < 10_000; j++) {
+                        usersRepository.save(User.builder().email("email").password("qwerty007").build());
+                        System.out.println(Thread.currentThread().getName() + " inserted " + j);
+                    }
+                });
+            }
+        }
+
+
+    }
+}
---
+++ lesson_04/code/Spring/src/main/java/de/ait/shop/repositories/impl/UsersRepositoryJdbcImpl.java
@@ -0,0 +1,72 @@
+package de.ait.shop.repositories.impl;
+
+import de.ait.shop.models.User;
+import de.ait.shop.repositories.UsersRepository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 9/11/2023
+ * Spring
+ *
+ * @author Marsel Sidikov (AIT TR)
+ */
+public class UsersRepositoryJdbcImpl implements UsersRepository {
+
+    //language=SQL
+    private static final String SQL_INSERT_ACCOUNT = "insert into account (email, password) values (?, ?);";
+
+    private final DataSource dataSource;
+
+    public UsersRepositoryJdbcImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(User model) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_INSERT_ACCOUNT)) {
+
+            statement.setString(1, model.getEmail());
+            statement.setString(2, model.getPassword());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows != 1) {
+                throw new SQLException("Cannot insert user");
+            }
+
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void update(User model) {
+
+    }
+
+    @Override
+    public User findOneByEmail(String email) {
+        return null;
+    }
+}
